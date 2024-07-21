643e8c56a3b6de13f5cd157026f19472c67a48aa
Ditipriya Gupta <mail.ditipriyagupta@gmail.com>
2024-07-21 22:39:49 +0530

    Fear : Added the list of players

--- src/PlayerList.jsx
+++ src/PlayerList.jsx
@@ -2,22 +2,24 @@
 import React from 'react'
 const players=[
     {
-        id:1,
+        id:2,
         name:"abc",
         score:"01:02:03"
     },
     {
-        id:2,
+        id:1,
         name:"bsd",
         score:"01:02:03"
     }
 ]
 
 const PlayerList = () => {
+  const sortedPlayerList=players.sort((a,b)=>(a.id>b.id?1:-1))
   return (
     <div>
-    {players.map((player)=>(
-        <div className='flex gap-2'>
+    {sortedPlayerList.map((player)=>(
+   
+        <div className='flex gap-2 even:bg-gray-400 odd:bg-yellow-400'>
         <p>{player.name}</p>
         <p>{player.score}</p>
         </div>
